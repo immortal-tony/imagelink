d076c78aa3f6f1254c3867c72ea25141c11b34e1
hongzf <hongzf@corp@21cn.com>
2020-12-14 10:55:29 +0800

    1.sql提交添加

---
+++ sql/mmc_gallery_features.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `mmc_gallery_features`
+(
+    `id`       int(12) NOT NULL COMMENT '模特的图片id',
+    `title`    varchar(64)  DEFAULT NULL COMMENT '标题',
+    `feature`  varchar(64)  DEFAULT NULL COMMENT '分类标签',
+    `pic_num`  varchar(64)  DEFAULT NULL COMMENT '图片数量',
+    `view_num` varchar(64)  DEFAULT NULL COMMENT '浏览数量',
+    `url`      varchar(200) DEFAULT NULL COMMENT '图片地址',
+    `comments` varchar(200) DEFAULT NULL COMMENT '评论描述',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
---
+++ sql/mmc_model.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `mmc_model`
+(
+    `id`            int(12) NOT NULL COMMENT '模特的id编号',
+    `name`          varchar(12)  DEFAULT NULL COMMENT '模特姓名',
+    `age`           varchar(12)  DEFAULT NULL COMMENT '模特年龄',
+    `birth`         varchar(36)  DEFAULT NULL COMMENT '模特生日',
+    `constellation` varchar(12)  DEFAULT NULL COMMENT '模特星座',
+    `stature`       int(4)       DEFAULT NULL COMMENT '模特身高',
+    `birtAddress`   varchar(36)  DEFAULT NULL COMMENT '模特出生地',
+    `url`           varchar(200) DEFAULT NULL COMMENT '模特封面图片地址',
+    `beatyTag`      varchar(12)  DEFAULT NULL COMMENT '美女分类',
+    `collectionId`  varchar(12)  DEFAULT NULL COMMENT '模特图集id号',
+    `comment`       varchar(200) DEFAULT NULL COMMENT '模特评论',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
