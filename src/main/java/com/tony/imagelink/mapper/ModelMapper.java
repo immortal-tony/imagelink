950a633fab357ed8ac64c3094d73923364bb06cf
hongzf <hongzf@corp.21cn.com>
2021-01-30 10:08:40 +0800

    3.描述

--- src/main/java/com/tony/imagelink/mapper/ModelMapper.java
+++ src/main/java/com/tony/imagelink/mapper/ModelMapper.java
@@ -8,7 +8,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import java.util.List;
 
 public interface ModelMapper{
+    /**
+     * 插入模特信息
+     * @param model
+     * @return
+     */
     int insert(Model model);
 
+    /**
+     * 选取模特的图集id和图集的模特特征描述，比如：feature=南初妹妹,秀人网,可爱,运动,
+     * @return
+     */
     List<Model> selectQueryModels();
 }
