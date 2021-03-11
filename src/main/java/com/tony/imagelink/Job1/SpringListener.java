881df895820fffadf30dcdd95b3c4c012905ff6f
hongzf <hongzf@corp.21cn.com>
2021-03-11 18:56:14 +0800

    3.3月11

--- src/main/java/com/tony/imagelink/Job1/SpringListener.java
+++ src/main/java/com/tony/imagelink/Job1/SpringListener.java
@@ -3,11 +3,19 @@ package com.tony.imagelink.Job1;
 import com.tony.imagelink.mapper.GalleryFeaturesMapper;
 import com.tony.imagelink.mapper.ModelMapper;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
@@ -19,6 +27,7 @@ import java.util.Timer;
  * @Date 2020/12/14 10:17
  */
 @Component
+@Slf4j
 public class SpringListener implements ApplicationListener<ApplicationReadyEvent> {
 
     @Autowired
@@ -38,5 +47,30 @@ public class SpringListener implements ApplicationListener<ApplicationReadyEvent
         Timer timer = new Timer();
         // 八天抓取一次
         timer.scheduleAtFixedRate(jobLink, time, 8 * 24 * 60 * 60 * 1000);
+
+//        addVideoUrl();
+    }
+
+    /**
+     * 添加上视频文件的方法函数
+     */
+    private void addVideoUrl() {
+        ClassPathResource resource = new ClassPathResource("requestUrl_20210311.txt");
+        SimpleDateFormat dft = new SimpleDateFormat("yyyyMMdd");
+        String today = dft.format(new Date());
+        // 历史的就不插入了
+        if (resource.getFilename().equals("requestUrl_" + today)) {
+            try {
+                DataInputStream ds = new DataInputStream(resource.getInputStream());
+                String str = null;
+//                if((str = ds.read()) != null){
+//
+//                }
+            } catch (Exception e) {
+                log.info("导入视频的地址失败", e);
+                e.printStackTrace();
+            }
+
+        }
     }
 }
